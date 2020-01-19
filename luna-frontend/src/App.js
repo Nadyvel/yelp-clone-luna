cca94f2bb0b3f1b4526a14e49ff8c82bb98b67fd
amir_g <info@ghani.ch>
2020-01-19 10:59:28 +0100

    initial commit frontend setup - not dockerised yet

---
+++ luna-frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+
+const App = () => {
+  return <div>App Component</div>;
+};
+
+export default connect()(App);
