055dc51e9d1f2571eb70fccc0aadb061411f4416
Nadja Gujer <velnady@gmail.com>
2020-01-20 14:06:12 +0100

    settings.py fixed

--- backend/restaurants/admin.py
+++
@@ -1,7 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-
-from restaurants.models import Restaurant, Comments
-
-admin.site.register(Restaurant)
-admin.site.register(Comments)
