a2afce457d69d277156bf6394b0e9cd36f18fb89
Nadja Gujer <velnady@gmail.com>
2020-01-20 15:35:28 +0100

    removed folder

--- luna-frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import { connect } from "react-redux";
-
-const App = () => {
-  return <div>App Component</div>;
-};
-
-export default connect()(App);
