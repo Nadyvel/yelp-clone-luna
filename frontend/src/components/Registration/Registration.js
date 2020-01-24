87b0a9e7a36bacfa853393e137b0fd1933d0fe67
amir_g <info@ghani.ch>
2020-01-24 13:46:07 +0100

    created restaurant details

--- frontend/src/components/Registration/Registration.js
+++ frontend/src/components/Registration/Registration.js
@@ -1,46 +1,44 @@
-import React, { useState } from "react";
-import { connect } from 'react-redux';
+import React, {useState} from "react";
+import {connect} from 'react-redux';
 import './Registration.css';
 import InputAndLabel from "../InputAndLabel/InputAndLabel";
 import OrangeButton from "../OrangeButton/OrangeButton";
-import { withRouter } from "react-router-dom";
+import {withRouter} from "react-router-dom";
+import {registrationAction} from "../../store/action/registrationAction";
 
 
 const Registration = props => {
     let [state, setState] = useState({
-      email: "",
+        email: "",
     });
 
     const handleChange = e =>
-        setState({ ...state, [e.target.name]: e.target.value });
-
-return(
-    <div className="registrationForm">
-        <div>
-        <h5 className="registrationTitle">REGISTRATION</h5>
-        <span className="headLine"></span>
-        </div>
-
-        <form className="registrationWrapper">
-
-            <div className="registrationRow1">
-                <InputAndLabel placeholder={"E-Mail address"} name="email" value={state.email} 
-                onChange={handleChange}/>
-            </div>
-            
-            <div className="registrationButton">
-                <OrangeButton content="Register"/> 
-            </div>
-        </form>
-    </div>
-)
-
-}
-
-const mapStateToProps = state => {
-    return {
-        tokens: state.loginReducer.tokens,
+        setState({...state, [e.target.name]: e.target.value});
+    const handleRegistration = async e => {
+        e.preventDefault();
+        const response = await props.dispatch(registrationAction(state.email));
+        if (Number(response.status) === 200) {
+            props.history.push("/congrats");
+        }
     }
+    return (
+        <div className="registrationForm">
+            <div>
+                <h5 className="registrationTitle">REGISTRATION</h5>
+                <span className="headLine"></span>
+            </div>
+            <form className="registrationWrapper" onSubmit={handleRegistration}>
+
+                <div className="registrationRow1">
+                    <InputAndLabel type="submit" placeholder={"E-Mail address"} name="email" value={state.email}
+                                   onChange={handleChange}/>
+                </div>
+                <div className="registrationButton">
+                    <OrangeButton content="Register"/>
+                </div>
+            </form>
+        </div>
+    )
 }
 
-export default withRouter(connect(mapStateToProps)(Registration))
+export default withRouter(connect()(Registration))
